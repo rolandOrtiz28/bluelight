88e09b84837e742129860f6a125b5d2df9d3cda1
Roland Ortiz <gwapohejunjo@gmail.com>
2024-11-15 23:01:47 +0700

    update

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -366,37 +366,37 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  document.addEventListener("DOMContentLoaded", function() {
-    // Register ScrollTrigger plugin
-    gsap.registerPlugin(ScrollTrigger);
+  // document.addEventListener("DOMContentLoaded", function() {
+   
+  //   gsap.registerPlugin(ScrollTrigger);
 
-    // About Section Scroll-Triggered Animation (Pan effect from sides)
-    gsap.from(".about-img", {
-      x: -200,
-      opacity: 0,
-      duration: 1.5,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: ".About-Section",
-        start: "top 80%",
-        toggleActions:'restart pause resume none',
-      }
-    });
+    
+  //   gsap.from(".about-img", {
+  //     x: -200,
+  //     opacity: 0,
+  //     duration: 1.5,
+  //     ease: "power2.out",
+  //     scrollTrigger: {
+  //       trigger: ".About-Section",
+  //       start: "top 80%",
+  //       toggleActions:'restart pause resume none',
+  //     }
+  //   });
 
-    gsap.from(".about-text", {
-      x: 200,
-      opacity: 0,
-      duration: 1.5,
-      ease: "power2.out",
-      delay: 0.5,
-      scrollTrigger: {
-        trigger: ".About-Section",
-        start: "top 80%",
-        toggleActions:'restart pause resume none',
-      }
-    });
+  //   gsap.from(".about-text", {
+  //     x: 200,
+  //     opacity: 0,
+  //     duration: 1.5,
+  //     ease: "power2.out",
+  //     delay: 0.5,
+  //     scrollTrigger: {
+  //       trigger: ".About-Section",
+  //       start: "top 80%",
+  //       toggleActions:'restart pause resume none',
+  //     }
+  //   });
 
-  });
+  // });
 
 
   document.querySelectorAll('.team-member').forEach(member => {
